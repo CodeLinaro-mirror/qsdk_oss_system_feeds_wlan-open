7577a0c3ae9b7f7a36cdd3b3f51fc94df588d885
Felix Fietkau <nbd@openwrt.org>
2017-05-09 14:35:02 +0530

    mac80211: fix HT mode selection for ad-hoc
    Change-Id: I2d5618be64e81eaf86e2eb5594e794fdc5d0da1c
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@43769 3c298f89-4303-0410-b956-a3cf2f4a3e73
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: Mohamme Shafi Shajakhan <mohammed@codeaurora.org>
    Signed-off-by: Tamizh chelvam <tamizhr@codeaurora.org>

--- mac80211/files/lib/netifd/wireless/mac80211.sh
+++ mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -549,7 +549,36 @@ mac80211_setup_adhoc() {
 	mcval=
 	[ -n "$mcast_rate" ] && hostapd_add_rate mcval "$mcast_rate"
 
-	iw dev "$ifname" ibss join "$ssid" $freq $htmode fixed-freq $bssid \
+	case "$htmode" in
+		VHT20|HT20) ibss_htmode=HT20;;
+		HT40*|VHT40|VHT80|VHT160)
+			case "$hwmode" in
+				a)
+					case "$(( ($channel / 4) % 2 ))" in
+						1) ibss_htmode="HT40+" ;;
+						0) ibss_htmode="HT40-";;
+					esac
+				;;
+				*)
+					case "$htmode" in
+						HT40+) ibss_htmode="HT40+";;
+						HT40-) ibss_htmode="HT40-";;
+						*)
+							if [ "$channel" -lt 7 ]; then
+								ibss_htmode="HT40+"
+							else
+								ibss_htmode="HT40-"
+							fi
+						;;
+					esac
+				;;
+			esac
+			[ "$auto_channel" -gt 0 ] && ibss_htmode="HT40+"
+		;;
+		*) ibss_htmode="" ;;
+	esac
+
+	iw dev "$ifname" ibss join "$ssid" $freq $ibss_htmode fixed-freq $bssid \
 		${beacon_int:+beacon-interval $beacon_int} \
 		${brstr:+basic-rates $brstr} \
 		${mcval:+mcast-rate $mcval} \
