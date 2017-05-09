23768c11cf667e85aa68876ede0b063a50c390f0
Felix Fietkau <nbd@openwrt.org>
2017-05-09 11:01:45 +0530

    mac80211: clean up ht capability handling, drop the use of the ht_capab list, use individual variables instead
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40682 3c298f89-4303-0410-b956-a3cf2f4a3e73
    
    Change-Id: I99df5e6113c72670f02c05f6fb3fbac5860a5974
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: Mohamme Shafi Shajakhan <mohammed@codeaurora.org>
    Signed-off-by: Tamizh chelvam <tamizhr@codeaurora.org>

--- mac80211/files/lib/wifi/mac80211.sh
+++ mac80211/files/lib/wifi/mac80211.sh
@@ -79,28 +79,8 @@ detect_mac80211() {
 		mode_band="a"
 		channel="36"
 		htmode=""
+		ht_capab=""
 
-		ht_cap=0
-		for cap in $(iw phy "$dev" info | grep 'Capabilities:' | cut -d: -f2); do
-			ht_cap="$(($ht_cap | $cap))"
-		done
-		ht_capab="";
-		[ "$ht_cap" -gt 0 ] && {
-			mode_11n="n"
-			htmode="HT20"
-
-			list="	list ht_capab"
-			[ "$(($ht_cap & 1))" -eq 1 ] && append ht_capab "$list	LDPC" "$N"
-			[ "$(($ht_cap & 16))" -eq 16 ] && append ht_capab "$list	GF" "$N"
-			[ "$(($ht_cap & 32))" -eq 32 ] && append ht_capab "$list	SHORT-GI-20" "$N"
-			[ "$(($ht_cap & 64))" -eq 64 ] && append ht_capab "$list	SHORT-GI-40" "$N"
-			[ "$(($ht_cap & 128))" -eq 128 ] && append ht_capab "$list	TX-STBC" "$N"
-			[ "$(($ht_cap & 768))" -eq 256 ] && append ht_capab "$list	RX-STBC1" "$N"
-			[ "$(($ht_cap & 768))" -eq 512 ] && append ht_capab "$list	RX-STBC12" "$N"
-			[ "$(($ht_cap & 768))" -eq 768 ] && append ht_capab "$list	RX-STBC123" "$N"
-			[ "$(($ht_cap & 4096))" -eq 4096 ] && append ht_capab "$list	DSSS_CCK-40" "$N"
-			[ "$(($ht_cap & 16384))" -eq 16384 ] && append ht_capab "$list	40-INTOLERANT" "$N"
-		}
 		iw phy "$dev" info | grep -q '5180 MHz' || { mode_band="g"; channel="11"; }
 		(iw phy "$dev" info | grep -q '5745 MHz' && !(iw phy "$dev" info | grep -q '5180 MHz')) && { mode_band="a"; channel="149"; }
 		iw phy "$dev" info | grep -q '60480 MHz' && { mode_11n="a"; mode_band="d"; channel="2"; }
