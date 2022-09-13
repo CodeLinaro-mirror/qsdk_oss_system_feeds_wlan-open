29db951f8cb3494eeb9bd3f0c4eb33a9c40551a5
Ramya Gnanasekar <quic_rgnanase@quicinc.com>
2022-09-13 22:31:36 +0530

    mac80211: update hostapd vht config based on vht capab
    VHT capability is updated with VHT160 only when both 160MHz
    and 80+80 bit is set in VHT phy capability information.
    
    Updating VHT160 to be configured in hostapd when 160MHz
    supported bit is set in phy capability information.
    When 80+80 MHz supported bit is set VHT160-80PLUS80 will be
    configured in hostapd configuration.
    
    Change-Id: I30f05e8e719193d4b78229b197427d9d0768a1e6
    Signed-off-by: Ramya Gnanasekar <quic_rgnanase@quicinc.com>

--- mac80211/files/lib/netifd/wireless/mac80211.sh
+++ mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -453,7 +453,7 @@ mac80211_hostapd_setup_base() {
 			vht160_hw=0
 			case "$htmode" in
 				VHT160|HE160|EHT160|EHT320)
-					[ "$(($vht_cap & 12))" -eq 8 -a 1 -le "$vht160" ] && \
+					[ "$(($vht_cap & 12))" -eq 4 -a 1 -le "$vht160" ] && \
 					vht160_hw=1
 					[ "$vht160_hw" = 1 ] && vht_capab="$vht_capab[VHT160]"
 					;;
