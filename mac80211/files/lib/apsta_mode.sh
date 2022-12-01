CodeLinaro-mirror/qsdk_oss_system_feeds_wlan-open

commit 862b6ef904eeeb5b446ebc69c79e31065815c4d4

mac80211: Script changes for channel switch
In reapter mode, to make AP interface switch to STA channel during
CSA, separate script is called. The script will be created in case
of 6GHz. When auto mode is configured, during APUT bringup, frequency
will not be known by the script, hence additional script to take care
of AP channel switch is not created which caused AP to not switch
channel.

To fix the issue, band is passed from mac80211.sh and used to create
the additional script for channel switch.

Hostapd cli will configure hw mode during AP bringup after channel
switch. hw mode is configured as "a" when channel number is greater than
36. When Root AP switches to 6Ghz channel less then 36 (say, 1, 5)
hw_mode is configured as "g" for 6GHz causing AP hostapd to fail.
Fixed the same by checking wifi gen value.

Updating the correct bitmap to validate whether EHT operational
parameter is present in IE

Change-Id: I083fb8b2f7d98c8ed5295b49a75b90385fdf7ef8
Signed-off-by: Ramya Gnanasekar <quic_rgnanase@quicinc.com>

mac80211/files/lib/apsta_mode.sh
@@ -18,6 +18,7 @@
 sta_intf="$1"
 ap_intf="$2"
 hostapd_conf="$3"
+oper_band="$4"
 dfs_log=1
 
 ap_ht_capab=$(cat $hostapd_conf 2> /dev/null | grep ht_capab | grep -v vht | cut -d'=' -f 2)
@@ -185,7 +186,7 @@ hostapd_adjust_config() {
 	fi
 	#echo "STA associated in Channel $sta_channel, Width $sta_width MHz, Wifi Gen $wifi_gen, ieee80211ac $ieee80211ac" > /dev/ttyMSM0
 	hostapd_cli -i $ap_intf set channel $sta_channel 2> /dev/null
-	if [ $sta_channel -ge 36 ]; then
+	if [ $sta_channel -ge 36 ] || [ $wifi_gen == 6 ] || [ $wifi_gen == 7 ]; then
 		hostapd_cli -i $ap_intf set hw_mode a 2> /dev/null
 	else
 		hostapd_cli -i $ap_intf set hw_mode g 2> /dev/null
@@ -295,7 +296,7 @@ hostapd_is_6ghz_band() {
 
 ap_freq=$(iw $ap_intf info 2> /dev/null | grep channel | cut -d' ' -f 3 | cut -b 2-5)
 wifi_6gband=$(hostapd_is_6ghz_band $ap_freq)
-if [ $wifi_6gband == true ]; then
+if [ $wifi_6gband == true ] || [ $oper_band == 3 ]; then
 cat > /lib/repeater_6g_ch_sw.sh << EOF
 #!/bin/sh
 

mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -1528,7 +1528,7 @@ drv_mac80211_setup() {
 
 	if [[ ! -z "$ap_ifname" && ! -z "$sta_ifname" && ! -z "$hostapd_conf_file" ]]; then
 		[ -f "/lib/apsta_mode.sh" ] && {
-			. /lib/apsta_mode.sh $sta_ifname $ap_ifname $hostapd_conf_file
+			. /lib/apsta_mode.sh $sta_ifname $ap_ifname $hostapd_conf_file $band
 			echo "$!" >> /tmp/apsta_mode.pid
 		}
 	fi

mac80211/patches/651-mac80211-Update-11be-EHT-elements-to-Draft-2.0-version.patch
@@ -19,8 +19,8 @@ Signed-off-by: Balamurugan Mahalingam <quic_bmahalin@quicinc.com>
 
 Index: b/include/linux/ieee80211.h
 ===================================================================
---- a/include/linux/ieee80211.h	2022-10-17 11:45:35.599543426 +0530
-+++ b/include/linux/ieee80211.h	2022-10-17 11:45:35.595543455 +0530
+--- a/include/linux/ieee80211.h	2022-11-18 18:35:15.600296202 +0530
++++ b/include/linux/ieee80211.h	2022-11-18 18:35:15.596296230 +0530
 @@ -1996,7 +1996,7 @@ struct ieee80211_eht_mcs_nss_supp_bw {
   * struct ieee80211_eht_cap_elem_fixed - EHT capabilities fixed data
   *
@@ -82,8 +82,8 @@ Index: b/include/linux/ieee80211.h
  
 Index: b/include/net/mac80211.h
 ===================================================================
---- a/include/net/mac80211.h	2022-10-17 11:45:35.599543426 +0530
-+++ b/include/net/mac80211.h	2022-10-17 11:45:35.595543455 +0530
+--- a/include/net/mac80211.h	2022-11-18 18:35:15.600296202 +0530
++++ b/include/net/mac80211.h	2022-11-18 18:35:15.600296202 +0530
 @@ -760,7 +760,8 @@ struct ieee80211_bss_conf {
  	bool eht_support;
  	struct {
@@ -96,8 +96,8 @@ Index: b/include/net/mac80211.h
  	bool ap_ps_enable;
 Index: b/net/mac80211/eht.c
 ===================================================================
---- a/net/mac80211/eht.c	2022-10-17 11:45:35.599543426 +0530
-+++ b/net/mac80211/eht.c	2022-10-17 11:45:35.595543455 +0530
+--- a/net/mac80211/eht.c	2022-11-18 18:35:15.600296202 +0530
++++ b/net/mac80211/eht.c	2022-11-18 18:35:15.600296202 +0530
 @@ -85,6 +85,7 @@ void ieee80211_eht_op_ie_to_bss_conf(str
  		return;
  
@@ -110,8 +110,8 @@ Index: b/net/mac80211/eht.c
  }
 Index: b/net/mac80211/mesh.c
 ===================================================================
---- a/net/mac80211/mesh.c	2022-10-17 11:45:35.599543426 +0530
-+++ b/net/mac80211/mesh.c	2022-10-17 11:45:35.595543455 +0530
+--- a/net/mac80211/mesh.c	2022-11-18 18:35:15.600296202 +0530
++++ b/net/mac80211/mesh.c	2022-11-18 18:35:15.600296202 +0530
 @@ -699,12 +699,14 @@ int mesh_add_eht_oper_ie(struct ieee8021
  		return 0;
  
@@ -157,8 +157,8 @@ Index: b/net/mac80211/mesh.c
  	bcn = kzalloc(sizeof(*bcn) + head_len + tail_len, GFP_KERNEL);
 Index: b/net/mac80211/mesh_plink.c
 ===================================================================
---- a/net/mac80211/mesh_plink.c	2022-10-17 11:45:35.599543426 +0530
-+++ b/net/mac80211/mesh_plink.c	2022-10-17 11:45:35.595543455 +0530
+--- a/net/mac80211/mesh_plink.c	2022-11-18 18:35:15.600296202 +0530
++++ b/net/mac80211/mesh_plink.c	2022-11-18 18:35:15.600296202 +0530
 @@ -222,6 +222,10 @@ static int mesh_plink_frame_tx(struct ie
  	u8 ie_len_he_cap, ie_len_eht_cap;
  	int hdr_len = offsetofend(struct ieee80211_mgmt, u.action.u.self_prot);
@@ -204,8 +204,8 @@ Index: b/net/mac80211/mesh_plink.c
  	if (!test_sta_flag(sta, WLAN_STA_RATE_CONTROL))
 Index: b/net/mac80211/spectmgmt.c
 ===================================================================
---- a/net/mac80211/spectmgmt.c	2022-10-17 11:45:35.599543426 +0530
-+++ b/net/mac80211/spectmgmt.c	2022-10-17 11:45:35.595543455 +0530
+--- a/net/mac80211/spectmgmt.c	2022-11-18 18:35:15.600296202 +0530
++++ b/net/mac80211/spectmgmt.c	2022-11-18 18:35:15.600296202 +0530
 @@ -152,7 +152,8 @@ int ieee80211_parse_ch_switch_ie(struct
  			eht_oper.chan_width = (eht_oper_params &
  					       IEEE80211_EHT_OPER_CHAN_WIDTH) |
@@ -218,8 +218,8 @@ Index: b/net/mac80211/spectmgmt.c
  					IEEE80211_HE_OPERATION_6GHZ_OP_INFO);
 Index: b/net/mac80211/util.c
 ===================================================================
---- a/net/mac80211/util.c	2022-10-17 11:45:35.599543426 +0530
-+++ b/net/mac80211/util.c	2022-10-17 11:46:23.859194636 +0530
+--- a/net/mac80211/util.c	2022-11-18 18:35:15.600296202 +0530
++++ b/net/mac80211/util.c	2022-11-18 18:37:02.219529000 +0530
 @@ -3293,27 +3293,50 @@ out:
  	return pos;
  }
@@ -366,7 +366,7 @@ Index: b/net/mac80211/util.c
  
 -	if (!eht_oper) {
 +	if (!eht_oper ||
-+	    !(eht_oper->ehtop_params & IEEE80211_EHT_OPER_PRESENT_BM)) {
++	    !(eht_oper->ehtop_params & EHTOP_PARAMS_EHTOP_INFORMATION_PRESENT)) {
  		switch (u8_get_bits(he_6ghz_oper->control,
  				    IEEE80211_HE_6GHZ_OPER_CTRL_CHANWIDTH)) {
  		case IEEE80211_HE_6GHZ_OPER_CTRL_CHANWIDTH_20MHZ:
@@ -412,8 +412,8 @@ Index: b/net/mac80211/util.c
  	if (!cfg80211_chandef_valid(&he_chandef)) {
 Index: b/net/mac80211/ieee80211_i.h
 ===================================================================
---- a/net/mac80211/ieee80211_i.h	2022-10-17 11:45:35.599543426 +0530
-+++ b/net/mac80211/ieee80211_i.h	2022-10-17 11:45:35.595543455 +0530
+--- a/net/mac80211/ieee80211_i.h	2022-11-18 18:35:15.600296202 +0530
++++ b/net/mac80211/ieee80211_i.h	2022-11-18 18:35:15.600296202 +0530
 @@ -2425,7 +2425,8 @@ u8 *ieee80211_ie_build_eht_cap(u8 *pos,
  			       const struct ieee80211_sta_he_cap *he_cap,
  			       const struct ieee80211_sta_eht_cap *eht_cap,

commit a00faff6b9ec07162948e71ff1d240c14ae4879e

mac80211: Fix 2G repeater mode not bring-up issue
While configuring the htmode as HE/EHT in 2G repeater
AP, wrongly setting the ap interface hardware mode as
'a' instead of 'g' due to an incorrect check condition.

Fix it by adding the operation mode check instead of wifi
generation check.

Fixes: 862b6ef90(mac80211: Script changes for channel switch)

Change-Id: I4e1252169d876e900768da06e12470a37d9f3dac
Signed-off-by: Sivashankari Madhavan <quic_sivamadh@quicinc.com>

mac80211/files/lib/apsta_mode.sh
@@ -186,7 +186,7 @@ hostapd_adjust_config() {
 	fi
 	#echo "STA associated in Channel $sta_channel, Width $sta_width MHz, Wifi Gen $wifi_gen, ieee80211ac $ieee80211ac" > /dev/ttyMSM0
 	hostapd_cli -i $ap_intf set channel $sta_channel 2> /dev/null
-	if [ $sta_channel -ge 36 ] || [ $wifi_gen == 6 ] || [ $wifi_gen == 7 ]; then
+	if [ $sta_channel -ge 36 ] || [ $oper_band == 3 ]; then
 		hostapd_cli -i $ap_intf set hw_mode a 2> /dev/null
 	else
 		hostapd_cli -i $ap_intf set hw_mode g 2> /dev/null