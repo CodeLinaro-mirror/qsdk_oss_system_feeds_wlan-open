CodeLinaro-mirror/qsdk_oss_system_feeds_wlan-open

commit 8d8db43bd4d54473c45e79df1559e7d72c331bfa

mac80211: fix adding dyndbg param to ath12k
Currently using sed, "ath12k" string will be replaced with "ath12k dyndbg=+p"
during boot. In subsequence boot, again the match string "ath12k" will be true
and it will be replaced. This will happen in every boot.

Hence, fix this by replacing the whole line instead of just matching "ath12k"
pattern.

Change-Id: Iecb9793286e5304e442db97490b2be7402def2fe
Signed-off-by: Aditya Kumar Singh <quic_adisi@quicinc.com>

mac80211/files/etc/init.d/ath12k_dyn_dbg_enable.sh
@@ -24,6 +24,6 @@ boot()
 	ath12k="/etc/modules.d/ath12k"
 	if [ -e $ath12k ];
 	then
-		sed -i 's/ath12k/ath12k dyndbg=+p/' $ath12k
+		sed -i '1s/.*/ath12k dyndbg=+p/' $ath12k
 	fi
 }

commit 1402a4e6be21b72f31f8c302dd49c6702752ceb9

mac80211.sh: fix over-writing full ath12k module insertion statement
Currently, if any module param is added in /etc/modules.d/ath12k for
boot time, it was being replaced with "ath12k dyndbg=+p". This happens
since script to add dyndbg param replaces full line every time during
boot.

Hence, fix this by first searching if "dyndbg" sub-string is present
already or not. And only if not present then replace "ath12k" with
"ath12k dyndbg=+p". This will make sure the other module params are
not overwritten.

Fixes: 8d8db43bd4 ("mac80211: fix adding dyndbg param to ath12k")

Change-Id: I22461d21eb33eb505580f17b1b3aacab0e8fc3db
Signed-off-by: Aditya Kumar Singh <quic_adisi@quicinc.com>

mac80211/files/etc/init.d/ath12k_dyn_dbg_enable.sh
@@ -24,6 +24,13 @@ boot()
 	ath12k="/etc/modules.d/ath12k"
 	if [ -e $ath12k ];
 	then
-		sed -i '1s/.*/ath12k dyndbg=+p/' $ath12k
+		# Read first line only
+		content=$(head -n 1 $ath12k)
+
+		# If first line does not have "dyndbg" substr in it
+		if [[ $content != *"dyndbg"* ]];
+		then
+			sed -i '1s/ath12k/ath12k dyndbg=+p/' $ath12k
+		fi
 	fi
 }