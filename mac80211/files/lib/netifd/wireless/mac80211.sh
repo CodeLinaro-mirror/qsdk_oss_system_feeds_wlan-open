CodeLinaro-mirror/qsdk_oss_system_feeds_wlan-open

commit 00a0b8db1030efbe24c297bda58b6491b7fb209e

mac80211.sh: Restrict WDS config when not supported
Currently we are not checking if device is in native wifi
or in eth encap mode when "option wds 1" is added in config.

WDS AP should not bring up if device is in eth encap mode.

Currently WDS is only supported on native wifi mode,
hence restricting it on eth encap mode.

Change-Id: Ib4040a990f06cfa188f8f2ce9ebc697c834773a2
Signed-off-by: Rajat Soni <quic_rajson@quicinc.com>

mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -737,6 +737,32 @@ EOF
 	json_select ..
 }
 
+mac80211_wds_support_check() {
+	local phy="$1"
+
+	local platform=$(grep -o "IPQ.*" /proc/device-tree/model | awk -F/ '{print $1}')
+	case "$platform" in
+		"IPQ8074" | "IPQ6018" | "IPQ5018")
+			frame_mode=$(cat /sys/module/ath11k/parameters/frame_mode)
+			;;
+		"IPQ9574")
+			local freq="$(get_freq "$phy" "$channel" "$device")"
+			local board_type=$(grep -o "IPQ.*" /proc/device-tree/model | awk -F/ '{print $2}' | awk -F- '{print $3}')
+
+			if [ $board_type == "C6" ] && [ $freq -gt 2000 ] && [ $freq -lt 3000]; then
+				frame_mode=$(cat /sys/module/ath11k/parameters/frame_mode)
+			else
+				frame_mode=$(cat /sys/module/ath12k/parameters/frame_mode)
+			fi
+			;;
+		 "IPQ5332")
+			frame_mode=$(cat /sys/module/ath12k/parameters/frame_mode)
+			;;
+	esac
+
+	echo "$frame_mode"
+}
+
 mac80211_hostapd_setup_bss() {
 	local phy="$1"
 	local ifname="$2"
@@ -773,7 +799,15 @@ mac80211_hostapd_setup_bss() {
 	set_default ftm_responder 0
 	set_default start_disabled 0
 
-	[ "$wds" -gt 0 ] && append hostapd_cfg "wds_sta=1" "$N"
+	if [ "$wds" -gt 0 ]; then
+		frame_mode=$(mac80211_wds_support_check "$phy")
+
+		if [ $frame_mode -ne 1 ]; then
+			echo WDS is supported only in native wifi mode. Kindly update the config > /dev/ttyMSM0
+			return
+		fi
+		append hostapd_cfg "wds_sta=1" "$N"
+	fi
 	[ "$ftm_responder" -gt 0 ] && append hostapd_cfg "ftm_responder=1" "$N"
 	[ "$staidx" -gt 0 -o "$start_disabled" -eq 1 ] && append hostapd_cfg "start_disabled=1" "$N"
 
@@ -1141,7 +1175,14 @@ mac80211_prepare_vif() {
 			[ -e $extsta_path ] && echo $extsta > $extsta_path
 			local wdsflag=
 			staidx="$(($staidx + 1))"
-			[ "$wds" -gt 0 ] && wdsflag="4addr on"
+			if [ "$wds" -gt 0 ]; then
+				frame_mode=$(mac80211_wds_support_check "$phy")
+				if [ $frame_mode -ne 1 ]; then
+					echo WDS is supported only in native wifi mode. Kindly update the config > /dev/ttyMSM0
+					return
+				fi
+				wdsflag="4addr on"
+			fi
 			mac80211_iw_interface_add "$phy" "$ifname" managed "$wdsflag" || return
 			if [ "$wds" -gt 0 ]; then
 				iw "$ifname" set 4addr on

commit ece7d0f657d7c712c7a770406998586f723f878b

mac80211.sh: Fix Restrict WDS config when not supported
Fix script issue in mac80211 while checking the frequency is in range of 2000Mhz
to 3000MHz.

Fixes: 00a0b8db1030 ("mac80211.sh: Restrict WDS config when not supported")

Change-Id: I1cb92a626f571a2851288e3bd09f32c516bf1686
Signed-off-by: Rajat Soni <quic_rajson@quicinc.com>

mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -753,7 +753,7 @@ mac80211_wds_support_check() {
 			local freq="$(get_freq "$phy" "$channel" "$device")"
 			local board_type=$(grep -o "IPQ.*" /proc/device-tree/model | awk -F/ '{print $2}' | awk -F- '{print $3}')
 
-			if [ $board_type == "C6" ] && [ $freq -gt 2000 ] && [ $freq -lt 3000]; then
+			if [ $board_type == "C6" ] && [ $freq -gt 2000 ] && [ $freq -lt 3000 ]; then
 				frame_mode=$(cat /sys/module/ath11k/parameters/frame_mode)
 			else
 				frame_mode=$(cat /sys/module/ath12k/parameters/frame_mode)