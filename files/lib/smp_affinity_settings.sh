0a2048bdbdb1130c5faca37b85cf01ae4749806a
Girish Kumar M P <quic_gmp@quicinc.com>
2023-10-16 23:17:10 +0530

    wifi: Add Boost configs and changed the Affinity for RDP442 and RDP446
    Currently there was no performance commands added for RDP442 and RDP446
    in the script and the Affinity was set for 8 Rings and there was only
    6 Rings.
    
    Fix the issue by adding the Boost commands in the boost_performance.sh
    script and also changed the affinity setting for
    6 rings and mapped to 3 cores.
    
    Change-Id: Ib868a0489d58fd48bc744cab6cd6cdc86aa8c146
    Signed-off-by: Girish Kumar M P <quic_gmp@quicinc.com>

--- files/lib/smp_affinity_settings.sh
+++ files/lib/smp_affinity_settings.sh
@@ -906,15 +906,14 @@ enable_affinity_mi01_3() {
 
         #QCN6432 WKK 5G/6G radio
         #pci 1
-        #assign 4 rx interrupts to each cores
-        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_4' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
+        #assign 3 rx interrupts to each cores
+        #rx interrupt,lmac,reo err,release interrupts are mapped to core 1
+        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_3' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 1 > /proc/irq/$irq_affinity_num/smp_affinity
-        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_5' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
+	irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_4' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 2 > /proc/irq/$irq_affinity_num/smp_affinity
-        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_6' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
+        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_5' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 4 > /proc/irq/$irq_affinity_num/smp_affinity
-        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_7' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
-        [ -n "$irq_affinity_num" ] && echo 8 > /proc/irq/$irq_affinity_num/smp_affinity
 
         #assign 3 tcl completions to last 3 CPUs
         irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_0' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
@@ -924,21 +923,16 @@ enable_affinity_mi01_3() {
         irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_2' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 4 > /proc/irq/$irq_affinity_num/smp_affinity
 
-        # lmac,reo err,release interrupts are mapped to core 3
-        irq_affinity_num=`grep -E -m1 'pci1_wlan_dp_3' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
-        [ -n "$irq_affinity_num" ] && echo 8 > /proc/irq/$irq_affinity_num/smp_affinity
-
 	#QCN6432 WKK 6G radio
         #pci 2
-	#assign 4 rx interrupts to each cores
-        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_4' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
+	#assign 3 rx interrupts to each cores
+	# rx interrupt,lmac,reo err,release interrupts are mapped to core 1
+        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_3' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 1 > /proc/irq/$irq_affinity_num/smp_affinity
-        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_5' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
+        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_4' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 2 > /proc/irq/$irq_affinity_num/smp_affinity
-        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_6' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
+        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_5' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 4 > /proc/irq/$irq_affinity_num/smp_affinity
-        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_7' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
-        [ -n "$irq_affinity_num" ] && echo 8 > /proc/irq/$irq_affinity_num/smp_affinity
 
         #assign 3 tcl completions to last 3 CPUs
         irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_0' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
@@ -948,9 +942,6 @@ enable_affinity_mi01_3() {
         irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_2' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
         [ -n "$irq_affinity_num" ] && echo 4 > /proc/irq/$irq_affinity_num/smp_affinity
 
-        # lmac,reo err,release interrupts are mapped to core 3
-        irq_affinity_num=`grep -E -m1 'pci2_wlan_dp_3' /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
-        [ -n "$irq_affinity_num" ] && echo 8 > /proc/irq/$irq_affinity_num/smp_affinity
 
 
 	enable_affinity_for_ds=$(cat /sys/module/cfg80211/parameters/g_bonded_interface_model)
@@ -1120,8 +1111,9 @@ enable_smp_affinity_wifi() {
                                         #for RDP468 (IPQ5332(2.4GHz) + QCN9274(5/6 GHz))
                                         enable_affinity_mi01_6
                                         ;;
-			ap-mi01.3)
-					#for RDP442(IPQ5332(2.4GHz) + QCN6432(5/6 GHz))
+			ap-mi01.3 | \
+			ap-mi04.1)
+					#for RDP442, RDP446 (IPQ5332(2.4GHz) + QCN6432(5/6 GHz)))
 					enable_affinity_mi01_3
 					;;
 			ap-mi01.9)
