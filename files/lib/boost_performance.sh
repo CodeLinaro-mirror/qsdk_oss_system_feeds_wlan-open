950b9042e1794096175f42f70aa550d37dee2bb7
gkarigan <quic_gkarigan@quicinc.com>
2023-10-26 14:02:14 -0700

    wifi: ath12k: Add Performance boost configs and smp affinity for RDP477 and RDP478
    Currently there are no performance commands supported in the boost
    performance.sh script as well as smp affinity settings for RDP477 and RDP478
    
    Updated the performance script with the relevant performance commands
    Updated the smp affinity settings for RDP477 and RDP478
    
    Change-Id: Ia02bae8fc2b7f9fa670dc0cc907aefa30f96b95f
    Signed-off-by: Ganesh Kariganuru Mahabalesh <quic_gkarigan@quicinc.com>

--- files/lib/boost_performance.sh
+++ files/lib/boost_performance.sh
@@ -620,6 +620,73 @@ boost_performance() {
 				tc qdisc replace dev wlan2 root noqueue
 				echo "16384" > /proc/net/skb_recycler/max_skbs
 				;;
+			ap-mi01.3-c2 | \
+			ap-mi04.1-c2)
+
+				tc qdisc replace dev eth0 root noqueue
+				tc qdisc replace dev eth1 root noqueue
+
+				ethtool -K eth0 gro off
+				ethtool -K eth0 gso off
+				ethtool -K eth1 gro off
+				ethtool -K eth1 gso off
+
+				ssdk_sh fdb learnCtrl set disable
+				ssdk_sh fdb entry flush 1
+
+				sysctl -w net.bridge.bridge-nf-call-ip6tables=1
+				sysctl -w net.bridge.bridge-nf-call-iptables=1
+
+				echo 1 > /sys/kernel/debug/ecm/ecm_db/defunct_all
+
+				/etc/init.d/firewall stop
+
+				echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+				echo "performance" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+				echo "performance" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+				echo "performance" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
+				#For 5GHz reo queues
+				echo 0x33333333 > /sys/kernel/debug/ath12k/qcn6432\ hw1.0_1/rx_hash_ix2
+				echo 0x23123123 > /sys/kernel/debug/ath12k/qcn6432\ hw1.0_1/rx_hash_ix3
+
+				#For 2GHz reo queues
+				echo 0x21212121 > /sys/kernel/debug/ath12k/ipq5332\ hw1.0_c000000.wifi/rx_hash_ix2
+				echo 0x23123123 > /sys/kernel/debug/ath12k/ipq5332\ hw1.0_c000000.wifi/rx_hash_ix3
+
+				echo 0 > /sys/kernel/debug/ath12k/qcn6432\ hw1.0_1/stats_disable
+				echo 1 > /sys/kernel/debug/ath12k/qcn6432\ hw1.0_1/stats_disable
+
+				echo 0 > /sys/kernel/debug/ath12k/ipq5332\ hw1.0_c000000.wifi/stats_disable
+				echo 1 > /sys/kernel/debug/ath12k/ipq5332\ hw1.0_c000000.wifi/stats_disable
+
+				echo 0 > /sys/class/net/wlan0/queues/rx-0/rps_cpus
+				echo 0 > /sys/class/net/wlan1/queues/rx-0/rps_cpus
+				echo 0 > /sys/class/net/wlan2/queues/rx-0/rps_cpus
+
+				if [ $(cat /sys/module/ath12k/parameters/ppe_ds_enable) -eq 1 ]; then
+					tc qdisc replace dev wlan0_b root noqueue
+					tc qdisc replace dev wlan0_l0 root noqueue
+					tc qdisc replace dev wlan0_l1 root noqueue
+					tc qdisc replace dev wlan0_l2 root noqueue
+
+					tc qdisc replace dev eth0 root noqueue
+					tc qdisc replace dev eth1 root noqueue
+					tc qdisc replace dev eth2 root noqueue
+					tc qdisc replace dev eth3 root noqueue
+					tc qdisc replace dev eth4 root noqueue
+					tc qdisc replace dev eth5 root noqueue
+
+					echo 1 > /sys/kernel/debug/ecm/ecm_db/defunct_all
+					echo f > /proc/net/nf_conntrack
+				fi
+
+				tc qdisc replace dev wlan0 root noqueue
+				tc qdisc replace dev wlan1 root noqueue
+				tc qdisc replace dev wlan2 root noqueue
+				echo "16384" > /proc/net/skb_recycler/max_skbs
+				#no settings
+				;;
 			*)
 				#no settings
 				;;
