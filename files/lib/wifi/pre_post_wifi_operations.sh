CodeLinaro-mirror/qsdk_oss_system_feeds_wlan-open

commit 89f1bfde50b3875c8b00fa0c4530adf79a499e5c

QSDK:CP: mac80211.sh: Fix the input validation issues in mlo link addition
Currently, if user gives the link addition command to add
a link which is already present in the interface, that link
will be added in the MLD which is incorrect and fix this issue
with validation check.

Also, change the mbssid value to 3 and add the mbssid_group_size
as 4 for 6GHz link addition.

Limitation:
We can't add a MLO link op top of SLO bring-up with /etc/config/
wireless file as wifi mlo_add_link expects the mld config in the
/etc/config/wireless to add a link.

Fixes: 9455dbeaba (mac80211.sh: Add support to handle mlo links addition)

Change-Id: I3bf4c27367aa28ce82ae931379202662b7ce8ceb
Signed-off-by: Manish Dharanenthiran <quic_mdharane@quicinc.com>
Signed-off-by: Maharaja Kennadyrajan <quic_mkenna@quicinc.com>

files/lib/wifi/pre_post_wifi_operations.sh
@@ -105,12 +105,21 @@ mlo_add_link() {
 	local key
 	local channels
 	local mld
+	local iface_data
+	local check_band
 
-	mld=$(uci show wireless | grep "$3" | cut -d "." -f 2)
+	if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+		echo "command line inputs missing" > /dev/ttyMSM0
+		return
+	fi
+
+	mld=$(uci show wireless | grep "'$3'" | cut -d "." -f 2)
 	[ -n "$mld" ] || {
 		echo "wrong interface name is given or mld doesn't found for given interface" > /dev/ttyMSM0
 		return
 	}
+
+	iface_data=$(uci show wireless | grep "'$mld'" | cut -d'.' -f2 | cut -d'@' -f 2)
 
 	case "$2" in
 		2g)
@@ -142,6 +151,13 @@ mlo_add_link() {
 		echo "failed to find band number" > /dev/ttyMSM0
 		return
 	}
+	for iface in $iface_data; do
+		check_band=$(uci show wireless.@${iface}.device | awk -F"'" '{print $2}' | cut -d'_' -f2)
+		if [ "$check_band" = "$link" ]; then
+			echo "link is already present in the mld" > /dev/ttyMSM0
+			return
+		fi
+	done
 	uci add wireless wifi-iface
 	conf_idx=$(uci show wireless | sed -n 's/.*@wifi-iface\[\([0-9]\+\)\].*/\1/p' | sort -n | tail -1)
 	echo 1 > /tmp/mlo_support.txt&
@@ -161,9 +177,9 @@ mlo_add_link() {
 	uci set wireless.@wifi-iface[$conf_idx].mld="$mld"
 	uci set wireless.@wifi-iface[$conf_idx].macaddr="$4"
 	uci commit wireless
-	input_file=/var/run/hostapd-${1}_${link}.conf
+	input_file=/var/run/hostapd-${1}.${link:${#link}-1:1}.conf
 	if [ -f $input_file ]; then
-		output_file=/tmp/hostapd-${1}_${link}.conf
+		output_file=/tmp/hostapd-${1}.${link:${#link}-1:1}.conf
 		if grep -q "bss=" "$input_file"; then
 			awk '/bss=/ {exit} {print}' "$input_file" > "$output_file"
 		else
@@ -179,6 +195,10 @@ mlo_add_link() {
 		fi
 		echo "bssid=$4" >> $output_file
 		echo "interface=$3" >> $output_file
+		if [ "6g" = "$2" ]; then
+			echo "mbssid=3" >> "$input_file"
+			echo "mbssid_group_size=4" >> "$input_file"
+		fi
 		hostapd_cli -i $3 mld_add_link bss_config=${1}:"$output_file"
 		rm "$output_file"
 	else
@@ -201,8 +221,8 @@ mlo_add_link() {
 		_wdev_handler_1 "$data" "mac80211" "setup" "radio0_$link" 2> /dev/null
 		json_select ..
 		if [ "6g" = "$2" ]; then
-			echo "mbssid=2" >> "$input_file"
-			echo "ema=1" >> "$input_file"
+			echo "mbssid=3" >> "$input_file"
+			echo "mbssid_group_size=4" >> "$input_file"
 		fi
 		hostapd_cli -i $3 mld_add_link bss_config=${1}:/var/run/hostapd-${1}_${link}.conf
 	fi

commit f6fa0c12fefa9339d24a22fb5af83bc31768aef4

QSDK:CP: scripts: Handle duplicate link addition with wifi command
1) Assign a name to the WiFi interface configuration
   (e.g., config wifi-iface 'default_radio0_band1') in the
   wireless configuration.
2) Remove the link dynamically using hostapd_cli.
3) Add the removed link dynamically using the wifi command.

This process results in the link being added, causing the wireless
configuration to have two wifi-ifaces under the same MLD in the
same radio. However the expectation is, the link should not be added
as the wireless config already has a link in that radio under that MLD.

In this same scenario, instead of adding the removed link back, if a
link is added in the radio where there is already a link configured
under that MLD, then it creates two vdevs in same radio under an MLD
which is an issue. Then during wifi down, when the links are
removed, the firmware asserts due to this two vdev scenario.

Fix this issue by removing the usage of the "@" character with
link addition, ensuring that any valid name given/not-given to the
wifi-iface works properly.

Fixes: 89f1bfde50b3 ("QSDK:CP: mac80211.sh: Fix the input validation issues in mlo link addition")

Change-Id: I0e6bc2d7a77a97f2d0ae89ce9ff9530e5c72440a
Signed-off-by: Nagarajan Maran <quic_nmaran@quicinc.com>

files/lib/wifi/pre_post_wifi_operations.sh
@@ -119,7 +119,7 @@ mlo_add_link() {
 		return
 	}
 
-	iface_data=$(uci show wireless | grep "'$mld'" | cut -d'.' -f2 | cut -d'@' -f 2)
+	iface_data=$(uci show wireless | grep "'$mld'" | cut -d'.' -f2)
 
 	case "$2" in
 		2g)
@@ -152,7 +152,7 @@ mlo_add_link() {
 		return
 	}
 	for iface in $iface_data; do
-		check_band=$(uci show wireless.@${iface}.device | awk -F"'" '{print $2}' | cut -d'_' -f2)
+		check_band=$(uci show wireless.${iface}.device | awk -F"'" '{print $2}' | cut -d'_' -f2)
 		if [ "$check_band" = "$link" ]; then
 			echo "link is already present in the mld" > /dev/ttyMSM0
 			return