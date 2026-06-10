151556bf306b7da146cab96338ff44bb4d32b7a2
Akriti Dinesh <adinesh@qti.qualcomm.com>
2026-06-10 12:26:03 +0530

    UPSTREAM:CP:wifi-scripts: Add rcac_freq UCI option support
    Introduce rcac_freq UCI option to allow configuring the Agile CAC
    operating frequency on the background radar chain
    
    Pass rcac_freq to hostapd base config when a non-zero value is set,
    enabling explicit frequency selection for RCAC operation
    
    Usage:
      /etc/config/wireless:
    
      config wifi-device
            option rcac_freq <channel>
    
    Patch-dependency:
    Iff4c782995d ("UPSTREAM:PROTOCOL:hostapd: Add bgcac_en config
    option for Agile CAC")
    I966e1cdfc895 ("UPSTREAM:CP:wifi-scripts: Add bgcac_en UCI option
    support")
    
    Change-Id: I68104eb1acbec4f64befd304177e646ad0969b02
    Signed-off-by: Akriti Dinesh <adinesh@qti.qualcomm.com>

--- wifi-scripts/files/lib/netifd/wireless/mac80211.sh
+++ wifi-scripts/files/lib/netifd/wireless/mac80211.sh
@@ -329,6 +329,7 @@ ubus_call() {
 		config_add_boolean sta_dfs_en
 		config_add_boolean use_driver_vendor_addr
 		config_add_boolean noscan ht_coex acs_exclude_dfs background_radar bgcac_en dfs_bw_reduce_en rpt_max_phy acs_2g_scan_all
+		config_add_int rcac_freq
 	# ACS behavior tuning
 	config_add_int acs_retry_interval acs_retry_count acs_periodic_interval acs_pcac_only
 	config_add_array ht_capab
@@ -927,11 +928,12 @@ mac80211_hostapd_setup_base() {
 		;;
 	esac
 	[ "$band" = "5g" ] && {
-		json_get_vars background_radar:0 bgcac_en:0 dfs_bw_reduce_en:0
+		json_get_vars background_radar:0 bgcac_en:0 dfs_bw_reduce_en:0 rcac_freq:0
 
 		[ "$background_radar" -eq 1 ] && append base_cfg "enable_background_radar=1" "$N"
 		[ "$bgcac_en" -eq 1 ] && append base_cfg "bgcac_en=1" "$N"
 		[ "$dfs_bw_reduce_en" -eq 1 ] && append base_cfg "dfs_bw_reduce_en=1" "$N"
+		[ "$rcac_freq" -gt 0 ] && append base_cfg "rcac_freq=$rcac_freq" "$N"
 	}
 
 	case "$htmode" in
